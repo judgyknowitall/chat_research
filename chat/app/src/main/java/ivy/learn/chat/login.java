02268f4b617c9e2af884511d9bbe14e29c0118e3
Zahra <zahra.ghavasieh@ucalgary.ca>
2020-06-29 09:00:57 -0600

    building login layout

--- chat/app/src/main/java/ivy/learn/chat/login.java
+++ chat/app/src/main/java/ivy/learn/chat/login.java
@@ -1,14 +1,14 @@
-package ivy.chat;
+package ivy.learn.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
